b69fe2e094cec35be93d341d7e802470ca57544d
Den <denowich@gmail.com>
2026-08-23 13:46:20 +0300

    Created components NewsBanner and Image, module css

---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,12 @@
+import { Image } from '../Image/Image';
+import styles from './styles.module.css';
+
+export const NewsBanner = ({ item }) => {
+  return (
+    <div className={styles.banner}>
+      <Image image={item?.image} />
+      <h3 className={styles.title}>NewsBanner</h3>
+      <p className={styles.author}>by author</p>
+    </div>
+  );
+};
--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -1,12 +1,11 @@
+import { NewsBanner } from '../../NewsBanner/NewsBanner';
 import styles from './styles.module.css';
 
 export const Main = () => {
   return (
     <main className={styles.main}>
       <h2>Main</h2>
-      <h3>Banner</h3>
-      <h3>image</h3>
-      <p>news</p>
+      <NewsBanner />
     </main>
   );
 };
