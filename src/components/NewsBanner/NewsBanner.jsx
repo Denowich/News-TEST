ef777c013ff851bbcf7ec9ccb8dd860b81c733b6
Den <denowich@gmail.com>
2026-09-01 13:26:26 +0300

    add env file

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -5,7 +5,7 @@ export const NewsBanner = ({ item }) => {
   return (
     <div className={styles.banner}>
       <Image image={item?.image} />
-      <h3 className={styles.title}>NewsBanner</h3>
+      <h3 className={styles.title}>NewsTitle</h3>
       <p className={styles.author}>by author</p>
     </div>
   );
